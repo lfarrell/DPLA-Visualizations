0276ea249186cdd697cfc472a12d0df8df2cfa40
Larry Farrell <farrelldlfarrell@gmail.com>
2014-01-13 21:35:12 -0500

    Added home site link

--- header.php
+++ header.php
@@ -4,5 +4,6 @@
         <li><a href="index.php">Term Frequency</a></li>
         <li><a href="institutions.php">Depositors</a></li>
         <li><a href="map.php">Searchable Map</a></li>
+        <li><a href="/">Back to Main Site</a> </li>
     </ul>
 </div>
