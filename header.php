299126490927e42928e97508e8a354564543fde5
Larry Farrell <farrelldlfarrell@gmail.com>
2015-01-05 19:10:27 -0500

    Add metadata explorer link

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
         <li><a href="institutions.php">Depositors</a></li>
         <li><a href="choropleth.php">Term Frequency Map</a></li>
         <li><a href="partners.php">Partner Flow</a></li>
+        <li><a href="/metadata_explorer">Metadata Explorer</a></li>
         <li><a href="/">Back to Main Site</a> </li>
     </ul>
 </div>
