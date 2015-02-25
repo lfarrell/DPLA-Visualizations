ea6afb74459b35805c31ff33632177c2c54ffe26
Larry Farrell <farrelldlfarrell@gmail.com>
2015-02-24 19:27:40 -0500

    Add western water link

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
         <li><a href="choropleth.php">Term Frequency Map</a></li>
         <li><a href="partners.php">Partner Flow</a></li>
         <li><a href="/metadata_explorer">Metadata Explorer</a></li>
-        li><a href="/western_water">Western Water</a></li>
+        <li><a href="/western_water">Western Water</a></li>
         <li><a href="/">Back to Main Site</a> </li>
     </ul>
 </div>
